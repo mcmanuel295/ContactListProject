0b4cedc3138d6f9fc8e50c695edebd514a9915ac
unknown <mcmanuel755@gmail.com>
2025-02-10 22:40:04 +0100

    update

--- src/main/java/com/example/ContactListProject/model/Contact.java
+++ src/main/java/com/example/ContactListProject/model/Contact.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
+@Setter
+@Getter
 @Entity
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @Table(name="contacts")
@@ -18,6 +22,8 @@ public class Contact {
     @Column(name = "id",unique = true,updatable = false)
     private String id;
     private String name;
+
+    @Email
     private String email ;
     private String title;
     private String phoneNumber;
@@ -28,67 +34,4 @@ public class Contact {
     private String photoUrl;
 
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPhoneNumber() {
-        return this.phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getStatus() {
-        return this.status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getPhotoUrl() {
-        return this.photoUrl;
-    }
-
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
 }
